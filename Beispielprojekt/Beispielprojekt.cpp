9b19b6ef8bb0f05b380d4bd69c945f5d5d8ece96
Pre-4lpha <ForWorknStuff@gmail.com>
2019-11-07 13:58:04 +0100

    Standard-Klassen erstellt

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -8,51 +8,101 @@
 #include <iostream>
 
 #include "Vektor2d.h"
+using namespace std;
 
-// Simulationsgeschwindigkeit
-const double DT = 100.0;
+int Fenster_x = 1920;
+int Fenster_y = 1080;
+int Zeit = 0;
+double rot_geschwindigkeit = 0.2;
+
+class Objekt {
+public:
+	int pos_x;
+	int pos_y;
+	signed int rel_pos_x;
+	signed int rel_pos_y;
+	signed int rel_pos()
+	{
+		this->rel_pos_x = (this->pos_x - Fenster_x / 2);
+		this->rel_pos_y = (this->pos_y - Fenster_y / 2);
+
+	}
+
+};
+
+class Earth  {
+
+	Gosu::Image Erde;
+
+public:
+	
+	Earth() :Erde("Erde.png") {
+
+	}
+	
+	int pos_x = Fenster_x / 2;
+	int pos_y = Fenster_y / 2;
+	void draw_erde()
+	{	
+		Erde.draw_rot(this->pos_x, this->pos_y, 0.0, Zeit * rot_geschwindigkeit, 0.5, 0.5);
+
+	}
+};
+
+
+class Rakete : public Objekt {
+public:
+	int Geschwindigkeit;
+	double rotation;
+	double rotation_berechnen() {
+		return(this->rotation = atan(unsigned (this->rel_pos_x) / unsigned (this->rel_pos_y)));
+		if (this->rel_pos_y < 0 && this->rel_pos_x >0)
+		{
+			this->rotation += 90;
+		}
+		else if (this->rel_pos_x < 0 && this->rel_pos_y < 0)
+		{
+			this->rotation += 180;
+		}
+		else if (this->rel_pos_x < 0 && this->rel_pos_y > 0)
+		{
+			this->rotation += 190;
+		}
+
+	}
+	double Bewegung() {
+		
+	}
+
+};
 
 class GameWindow : public Gosu::Window
 {
 public:
-	Gosu::Image bild;
+	Gosu::Image Raumschiff;
+
+	Earth erde;
 	GameWindow()
-		: Window(800, 600)
-		, bild("rakete.png")
+		: Window(Fenster_x, Fenster_y),
+		Raumschiff("rakete.png")
 	{
-		set_caption("Gosu Tutorial Game mit Git");
+		set_caption("Earth");
 	}
 	int x = 0;
-	int y = 0;
-	int rot_x = 0;
-	int rot_y = 0;
-
 	// wird bis zu 60x pro Sekunde aufgerufen.
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	void draw() override
 	{
-		bild.draw_rot(x, y, 0.0,
-			rot_y,
-			0.5, 5*sin(1+rot_x*0.1));
+		erde.draw_erde();
 	}
 
 	// Wird 60x pro Sekunde aufgerufen
 	void update() override
 	{
-		y = input().mouse_y();
-		x = input().mouse_x();
+		x += 1;
 		if (input().down(Gosu::MS_LEFT)) {
-			rot_y -= 5;
-		}
-		if (input().down(Gosu::MS_RIGHT)) {
-			rot_y += 5;
-		}
-		if (input().down(Gosu::MS_RIGHT)) {
-			rot_x += 1;
-		}
-		if (input().down(Gosu::MS_LEFT)) {
-			rot_x -= 1;
+		
 		}
 	}
 };
@@ -60,6 +110,9 @@ public:
 // C++ Hauptprogramm
 int main()
 {
+	
 	GameWindow window;
 	window.show();
+
+
 }
