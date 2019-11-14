9fcaed98c6e991fa6b368e2750395fe0c02e78c2
Pre-4lpha <ForWorknStuff@gmail.com>
2019-11-14 15:36:56 +0100

    Obejkt Klasse überarbeitet. Bewegung, Rotation und Draw definiert

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <string>
 #include <iostream>
+#define PI 3.14159265
 
 #include "Vektor2d.h"
 using namespace std;
@@ -26,75 +27,95 @@ double deltax = 0.0;
 double deltay = 0.0;
 double deltad = 0.0;
 double reichweite = 1000;
+double rot(Objekt x, Objekt y) {}
 //*************************************end
 
 class Objekt {
 public:
-	int pos_x;
-	int pos_y;
-	signed int rel_pos_x;
-	signed int rel_pos_y;
-	signed int rel_pos()
+	Gosu::Image Bild;
+	double pos_x;
+	double pos_y;
+	double rot;
+	int Ziel_x;
+	int Ziel_y;
+	Objekt* Target_Objekt_Ptr;
+	Objekt(int x, int y, Objekt* oz, Gosu::Image B) : pos_x(x), pos_y(y), Target_Objekt_Ptr(oz), Bild(B) {};
+	double dy;
+	double dx;
+	double abstand;
+
+	void abstand_berechnen() {
+		double dy = (this->pos_y - this->Target_Objekt_Ptr->pos_y);
+		if (dy == 0)
+			dy += 0,01;
+		double dx = (this->pos_x - this->Target_Objekt_Ptr->pos_x);
+		if (dx == 0)
+			dx += 0, 01;
+		abstand = sqrt(dy*dy + dx*dx);
+
+	};
+	void rot_berechnen() {
+		
+		this->rot = atan(unsigned (this->dy)/unsigned (this->dx)) * 180 / PI;;
+		if ((this->dy && this->dx) < 0) {
+			this->rot = this->rot *(-1);
+		}
+		if ((this->dy) < 0) {
+			this->rot = this->rot *(-1);
+		}
+		if ((this->dx) < 0) {
+			this->rot = 90+(90-this->rot);
+		}
+		
+	};
+	void bewegung() {
+		this->pos_x = this->dx + ((dx / abstand) * 8);
+		this->pos_y = this->dy + ((dy / abstand)) * 8);
+	}
+	void draw()
 	{
-		this->rel_pos_x = (this->pos_x - Fenster_x / 2);
-		this->rel_pos_y = (this->pos_y - Fenster_y / 2);
+		Bild.draw_rot(this->pos_x, this->pos_y, 0.0, rot, 0.5, 0.5);
 
 	}
 
+
 };
 
-class Earth  {
 
-	Gosu::Image Erde;
+
+class Erde : public Objekt  {
+
+	Gosu::Image Bild;
 
 public:
 	
-	Earth() :Erde("Erde.png") {
+	Erde(int x, int y, Objekt* oz, Gosu::Image B): Objekt(x,y,oz,B)  {
 
 	}
 	
 	int pos_x = Fenster_x / 2;
 	int pos_y = Fenster_y / 2;
-	void draw_erde()
-	{	
-		Erde.draw_rot(this->pos_x, this->pos_y, 0.0, Zeit * rot_geschwindigkeit, 0.5, 0.5);
-
-	}
+	
 };
+
+
 
 
 class Raumschiff : public Objekt {
 public:
 	int Geschwindigkeit;
-	double rotation;
-	double rotation_berechnen() {
-		return(this->rotation = atan(unsigned (this->rel_pos_x) / unsigned (this->rel_pos_y)));
-		if (this->rel_pos_y < 0 && this->rel_pos_x >0)
-		{
-			this->rotation += 90;
-		}
-		else if (this->rel_pos_x < 0 && this->rel_pos_y < 0)
-		{
-			this->rotation += 180;
-		}
-		else if (this->rel_pos_x < 0 && this->rel_pos_y > 0)
-		{
-			this->rotation += 190;
-		}
-
-	}
-	double Bewegung() {
-		
-	}
+	
+	
 
 };
+	
 
 class GameWindow : public Gosu::Window
 {
 public:
 	Gosu::Image Raumschiff;
 	
-	Earth erde;
+
 	GameWindow()
 		: Window(Fenster_x, Fenster_y),
 		Raumschiff("rakete.png")
@@ -107,38 +128,10 @@ public:
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
 	void draw() override
 	{
-		erde.draw_erde();
-		//*************************************start
-		Raumschiff.draw(x_maus, y_maus, 0.0,
-			1.0, // Rotationswinkel in Grad
-			1.0 // Position der "Mitte" relativ zu x, y (*20=Pixel)
-		);
-		if (misexist == true)
-		{
-			if (misx == 0 || misy == 0)
-			{
-				misx = 100;
-				misy = 100;
-			}
-			deltax = x_maus - misx;
-			deltay = y_maus - misy;
-			deltad = sqrt((y_maus - misy) * (y_maus - misy) + (x_maus - misx) * (x_maus - misx));
-			misxnew = misx + ((deltax / deltad) * 8);
-			misynew = misy + ((deltay / deltad) * 8);
-			//rot = (double)(((int)(270 + asin(deltay / deltad))) % 360);
-			//rot = 45.0;
-			reichweite = reichweite - sqrt((misynew - misy) * (misynew - misy) + (misxnew - misx) * (misxnew - misx));
-			if (reichweite <= 0 || deltad <= 30)
-			{
-				misexist = false;
-				reichweite = 1000;
-			}
-			Raumschiff.draw_rot(misxnew, misynew, 0.0, 45.0, 0.0);
-			misx = misxnew;
-			misy = misynew;
-		}
-		//*************************************end
+
 	}
+
+
 
 	// Wird 60x pro Sekunde aufgerufen
 	void update() override
